66f944d2f51a993bb1b0de1563e6e31088b57c34
Tyler Fridley <email": null>
2017-07-27 17:04:00 -0500

    Add user registration routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ get '/users/new' do
 end
 
 post '/users' do
+  if params[:password] == params[:confirm_password]
+    new_user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if new_user.save
+      redirect '/'
+    else
+      @errors = new_user.errors.full_messages
+      erb :'/users/new'
+    end
+  else
+    @errors = ["Passwords must match."]
+    erb :'/users/new'
+  end
 end
