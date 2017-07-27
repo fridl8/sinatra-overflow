81c63a37bf447dcdd4bb554196c09aab6ca79301
mwmayerle <mwmayerle@gmail.com>
2017-07-27 17:17:33 -0500

    login route and form complete. Cannot test yet. Errors partial complete and tesing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ post '/sessions/login' do
 	user = User.find_by(email: params[:user][:email])
 	if user && user.authenticate(params[:user][:password], params[:user][:email])
 		session[:user_id] = user.id
+		redirect '/'
 	else
 		@errors = ["Could not log in"]
 		erb :'/sessions/login'
