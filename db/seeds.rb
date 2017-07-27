f0e7805fbf4959611089986f39d8c93a9e1f8850
Brendan O'Handley <Brendan.ohandley@gmail.com>
2017-07-27 16:30:00 -0500

    add styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Answer.delete_all
 Vote.delete_all
 
 50.times do
-  User.create(username: Faker::Internet.user_name, password_hash: rand(1..9), email: Faker::Internet.email)
+  User.create(username: Faker::Internet.user_name, password: rand(1..9), email: Faker::Internet.email)
 end
 
 250.times do
@@ -22,7 +22,7 @@ end
   Answer.create(body: Faker::RickAndMorty.quote, responder_id: rand(1..50), question_id: rand(1..250))
 end
 
-500.times do
+5000.times do
   Vote.create(voter_id: rand(1..50), votable_id: rand(1..250), votable_type: COMMENT_AND_VOTE_TYPES.sample)
 end
 
