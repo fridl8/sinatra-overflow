a541106ab6bbea5420c2db8b54fd0ca5af616788
Tyler Fridley <email": null>
2017-07-28 11:17:30 -0500

    Add jquery and ajax for new question form

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  newQuestionSubmit();
 });
+
+var newQuestionSubmit = function() {
+  $('.new-question').on('submit', '#new-question-form', function(event) {
+    event.preventDefault();
+    var questionTitle = $('#new-question-form').find('input').first().val();
+    var questionBody = $('#new-question-form').find('textarea').val();
+    console.log(questionBody);
+    $.ajax({
+      method: "post",
+      url: "/questions",
+      data: {title: questionTitle, body: questionBody}
+    }).done(function(newQuestionId) {
+      console.log(newQuestionId);
+      var newQuestionUrl = "http://localhost:9393/questions/"+newQuestionId.question_id
+      console.log(newQuestionId)
+      if(newQuestionId != null || newQuestionId != undefined) {
+        window.location = newQuestionUrl
+      } else{
+        window.location = "http://localhost:9393/questions"
+      }
+    })
+  });
+}
