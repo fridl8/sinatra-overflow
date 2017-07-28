033eba587156a3ff05fe3b47c447f07b20f2aebb
Tyler Fridley <email": null>
2017-07-28 11:15:53 -0500

    Add route for making new question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -8,3 +8,23 @@ get "/questions/:id" do
   @question = Question.find_by(id: params[:id])
   erb :"questions/show"
 end
+
+post "/questions" do
+  if logged_in?
+    if request.xhr?
+      new_question = Question.new(title: params[:title], inquirer_id: session[:user_id], body: params[:body])
+      if new_question.save
+        @question = new_question
+        content_type :json
+        {question_id: @question.id}.to_json
+      else
+        @errors = new_question.errors.full_messages
+        erb :'/questions/index'
+      end
+    else
+      error 422
+    end
+  else
+    error 422
+  end
+end
