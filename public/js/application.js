c5f4d3e577e112105eacfd88de0ff1893464118c
Tyler Fridley <email": null>
2017-07-28 14:54:13 -0500

    Add beginning of new question comment js and ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   newQuestionButtonClickEvent();
   newQuestionSubmit();
+  newQuestionCommentButtonClickEvent();
 });
-
+// Functions
 var newQuestionButtonClickEvent = function() {
   $('#toggle-question-form').on("click", function(event) {
     event.preventDefault();
-
     $(event.target).toggle();
     $(".new-question").toggle();
   });
@@ -26,6 +26,31 @@ var newQuestionSubmit = function() {
         window.location = newQuestionUrl
     }).fail(function(errors){
       alert("Title or body can't be blank");
+    })
+  });
+}
+
+var newQuestionCommentButtonClickEvent = function() {
+  $('#toggle-q-comment-form').on('click', function(event) {
+    event.preventDefault();
+    $(event.target).toggle();
+    $('.question-comments').toggle();
+  });
+}
+
+var newQuestionCommentSubmit = function() {
+  $('.question-comments').on('submit', '.new-comment-form', function(){
+    event.preventDefault();
+    var commentType = 'question'
+    var questionCommentBody = $('#new-comment-form').find('textarea').val();
+    $.ajax({
+      method: "post",
+      url: "/comments",
+      data: {body: questionCommentBody, comment_type: commentType}
+    }).done(function(newCommentObject) {
+
+    }).fail(function(errors){
+      alert("Comment body can't be blank")
     })
   });
 }
