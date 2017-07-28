f0e3a40676ebe28b9c8aa1ebe3a1099c1bbf4ad8
Tyler Fridley <email": null>
2017-07-28 14:51:08 -0500

    Add partial route for new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,1 +1,22 @@
+post '/comments' do
+  if logged_in?
+    if lsdflj == 'question'
+    if request.xhr?
+      if new_comment.save
 
+      else
+        status 422
+      end
+    else
+      if new_comment.save
+      else
+        status 422
+        @errors = new_comment.errors.full_messages
+        erb :"/questions/#{q_id}"
+      end
+    end
+  else
+    @errors = ["You need to be logged in"]
+    erb :'/sessions/login'
+  end
+end
