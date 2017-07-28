536312c6e671d5b4189e6e8207fd62a3e846c2e3
Tyler Fridley <email": null>
2017-07-28 12:37:54 -0500

    Refactor jquery and ajax for new question

--- public/js/application.js
+++ public/js/application.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
+  newQuestionButtonClickEvent();
   newQuestionSubmit();
 });
+
+var newQuestionButtonClickEvent = function() {
+  $('#toggle-question-form').on("click", function(event) {
+    event.preventDefault();
+
+    $(event.target).toggle();
+    $(".new-question").toggle();
+  });
+}
 
 var newQuestionSubmit = function() {
   $('.new-question').on('submit', '#new-question-form', function(event) {
     event.preventDefault();
     var questionTitle = $('#new-question-form').find('input').first().val();
     var questionBody = $('#new-question-form').find('textarea').val();
-    console.log(questionBody);
     $.ajax({
       method: "post",
       url: "/questions",
       data: {title: questionTitle, body: questionBody}
     }).done(function(newQuestionId) {
-      console.log(newQuestionId);
       var newQuestionUrl = "http://localhost:9393/questions/"+newQuestionId.question_id
-      console.log(newQuestionId)
-      if(newQuestionId != null || newQuestionId != undefined) {
         window.location = newQuestionUrl
-      } else{
-        window.location = "http://localhost:9393/questions"
-      }
+    }).fail(function(errors){
+      alert("Title or body can't be blank");
     })
   });
 }
