aa96659f42f2aec2e9eafd6617e8bb5a1ceb669e
Tyler Fridley <email": null>
2017-07-28 16:43:16 -0500

    Add to_json for object

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,44 @@
 post '/comments' do
   if logged_in?
-    if lsdflj == 'question'
-    if request.xhr?
-      if new_comment.save
-
+    if params[:comment_type] == "question"
+      current_question = Question.find_by(id: params[:current_question_id].to_i)
+      if request.xhr?
+        if current_question.comments << Comment.new(body: params[:body], commenter_id: session[:user_id]) == false
+          status 422
+        else
+          content_type :json
+          {commenter_id: session[:user_id], commentable_id: params[:current_question_id].to_i, commentable_type: 'Question'}.to_json
+        end
       else
-        status 422
+        if current_question.comments << Comment.new(body: params[:body], commenter_id: session[:user_id]) == false
+          status 422
+          @errors = ["Something went wr0ng :'("]
+          erb :"/questions/#{current_question.id}"
+        else
+          redirect "/questions/#{current_question.id}"
+        end
       end
     else
-      if new_comment.save
+      current_answer = Answer.find_by(id: params[:current_answer_id].to_i)
+      if request.xhr?
+        if current_answer.comments << Comment.new(body: params[:body], commenter_id: session[:user_id]) == false
+          status 422
+        else
+          content_type :json
+          {commenter_id: session[:user_id], commentable_id: params[:current_answer_id], commentable_type: 'Answer'}.to_json
+        end
       else
-        status 422
-        @errors = new_comment.errors.full_messages
-        erb :"/questions/#{q_id}"
+        if current_answer.comments << Comment.new(body: params[:body], commenter_id: session[:user_id]) == false
+          status 422
+          @errors = ["Something went wr0ng :'("]
+          erb :"/questions/#{current_answer.question.id}"
+        else
+          redirect "/questions/#{current_answer.question.id}"
+        end
       end
     end
   else
-    @errors = ["You need to be logged in"]
+    @errors = ["You nEed to be logged in!"]
     erb :'/sessions/login'
   end
 end
