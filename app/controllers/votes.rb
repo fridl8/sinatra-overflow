345759d097942757f52c36f8841a7d88ea04f69d
Brendan O'Handley <Brendan.ohandley@gmail.com>
2017-07-29 10:51:59 -0500

    debug vote button. Allow user to upvote and downvote and display vote sum to DOM

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -4,41 +4,34 @@ post "/questions/:id/votes" do
   # otherwise create a new vote for this question
   # by this user and give it a value according
   # to what button they pressed
-  
+
   if !logged_in?
     status 401
   else
     @question = Question.find_by(id: params[:id])
     if request.xhr?
+      # if a vote does not exist on this question for this user
       if !@question.votes.find_by(voter_id: current_user.id)
         if params[:vote] == '1'
-          p "P" * 20
           @question.votes << Vote.new(voter_id: current_user.id, value: 1)
-          p @question.sum_vote_values.to_s
+          @question.sum_vote_values.to_s
         elsif params[:vote] == '-1'
-          p "Z" * 20
           @question.votes << Vote.new(voter_id: current_user.id, value: -1)
-          p @question.sum_vote_values.to_s
+          @question.sum_vote_values.to_s
         end
       else
         @vote = @question.votes.find_by(voter_id: current_user.id)
         if params[:vote] == '1'
-          p "X" * 20
-          p @vote
           @vote.value = 1
-          p @vote
-          p @question.sum_vote_values.to_s
+          @vote.save
+          @question.sum_vote_values.to_s
         elsif params[:vote] == '-1'
-          p "Q" * 20
-          p @vote
           @vote.value = -1
-          p @vote
-          p @question.sum_vote_values.to_s
+          @vote.save
+          @question.sum_vote_values.to_s
         else
-          p "Y" * 20
-          p @vote
           @vote.delete
-          p @question.sum_vote_values.to_s
+          @question.sum_vote_values.to_s
         end
       end
     end
